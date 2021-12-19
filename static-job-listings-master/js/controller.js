e1ed6aade2d065ffb86e34cd4d73438a245f851a
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-12-19 09:35:31 +0100

    start javascript part

---
+++ static-job-listings-master/js/controller.js
@@ -0,0 +1,214 @@
+console.log('controller')
+
+const jobsListings = [
+    {
+      "id": 1,
+      "company": "Photosnap",
+      "logo": "./images/photosnap.svg",
+      "new": true,
+      "featured": true,
+      "position": "Senior Frontend Developer",
+      "role": "Frontend",
+      "level": "Senior",
+      "postedAt": "1d ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["HTML", "CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 2,
+      "company": "Manage",
+      "logo": "./images/manage.svg",
+      "new": true,
+      "featured": true,
+      "position": "Fullstack Developer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1d ago",
+      "contract": "Part Time",
+      "location": "Remote",
+      "languages": ["Python"],
+      "tools": ["React"]
+    },
+    {
+      "id": 3,
+      "company": "Account",
+      "logo": "./images/account.svg",
+      "new": true,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2d ago",
+      "contract": "Part Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    },
+    {
+      "id": 4,
+      "company": "MyHome",
+      "logo": "./images/myhome.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "5d ago",
+      "contract": "Contract",
+      "location": "USA Only",
+      "languages": ["CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 5,
+      "company": "Loop Studios",
+      "logo": "./images/loop-studios.svg",
+      "new": false,
+      "featured": false,
+      "position": "Software Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["Ruby", "Sass"]
+    },
+    {
+      "id": 6,
+      "company": "FaceIt",
+      "logo": "./images/faceit.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Backend Developer",
+      "role": "Backend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "UK Only",
+      "languages": ["Ruby"],
+      "tools": ["RoR"]
+    },
+    {
+      "id": 7,
+      "company": "Shortly",
+      "logo": "./images/shortly.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["HTML", "JavaScript"],
+      "tools": ["Sass"]
+    },
+    {
+      "id": 8,
+      "company": "Insure",
+      "logo": "./images/insure.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["Vue", "Sass"]
+    },
+    {
+      "id": 9,
+      "company": "Eyecam Co.",
+      "logo": "./images/eyecam-co.svg",
+      "new": false,
+      "featured": false,
+      "position": "Full Stack Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "3w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript", "Python"],
+      "tools": ["Django"]
+    },
+    {
+      "id": 10,
+      "company": "The Air Filter Company",
+      "logo": "./images/the-air-filter-company.svg",
+      "new": false,
+      "featured": false,
+      "position": "Front-end Dev",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "1mo ago",
+      "contract": "Part Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    }
+]
+
+function renderJobListings() {
+    return jobsListings.map(renderJob).join('')
+}
+
+function renderJob(jobItem) {
+    console.log('jobItem: ', jobItem)
+    return `
+    <section class="job-post job-post--featured">
+        <img src="${jobItem.logo}" alt="${jobItem.company} logo" class="job-post__logo">
+        <div class="job-post__info">
+            <p class="job-post__company">
+                ${jobItem.company}
+                <span class="tag tag--new">
+                    ${jobItem.new}
+                </span>
+                <span class="tag tag--featured">
+                    ${jobItem.featured}
+                </span>
+            </p>
+            <h3 class="job-post__title">
+                ${jobItem.position}
+            </h3>
+            <p class="job-post__description">
+                <span class="job-post__release-date">
+                    ${jobItem.postedAt}
+                </span>
+                <span class="job-post__type">
+                    ${postedAt.contract}
+                </span>
+                <span class="job-post__access">
+                    ${postedAt.location}
+                </span>
+            </p>
+        </div>
+        <div class="separator"></div>
+        <div class="job-post__tags">
+            <ul class="label__list">
+                <li class="label">
+                    Frontend
+                </li>
+                <li class="label">
+                    Senior
+                </li>
+                <li class="label">
+                    HTML
+                </li>
+                <li class="label">
+                    CSS
+                </li>
+                <li class="label">
+                    JavaScript
+                </li>
+            </ul>
+        </div>
+    </section>
+    `
+}
+
+renderJobListings()
