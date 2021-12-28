0e612193f3ac63599b28a08b011b3a2c8fd7bab3
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-12-28 08:37:56 +0100

    design the general layout

--- todo-app-main-vue/src/App.vue
+++ todo-app-main-vue/src/App.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>Hey</div>
+  <TheHeader />
+  <div class="container">
+    <div class="wrapper">
+      <div class="todo">
+        <h2>Todo</h2>
+        <span>moon</span>
+      </div>
+      <input type="text" />
+    </div>
+  </div>
 </template>
 
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+  setup() {},
+};
+</script>
+
 <style lang="scss">
-div {
-  color: red;
+.container {
+  margin: 0 auto;
+  display: grid;
+  max-width: var(--container-max-width);
+  grid-template-columns:
+    [full-start] 1fr
+    [center-start] repeat(12, [col-start]6.5rem [col-end])
+    [center-end] 1fr [full-end];
+  gap: var(--gutter);
+}
+
+.wrapper {
+  grid-column: col-start 4 / span 6;
+}
+
+.container {
+  grid-column: full-start / full-end;
 }
 </style>
